9b05b91f6a8f16fc9a27bf80937da1920ae8581f
Nicole Honl <nicole.m.honl@gmail.com>
2024-06-14 16:44:51 -0500

    updated db to run

--- backend/db.js
+++ backend/db.js
@@ -1,23 +1,24 @@
-"use strict";
-/** Database setup for jobly. */
+// "use strict";
+// /** Database setup for jobly. */
+// const { Client } = require("pg");
+// const { getDatabaseUri } = require("./config");
+
 const { Client } = require("pg");
-const { getDatabaseUri } = require("./config");
 
-let db;
+let DB_URI;
 
-if (process.env.NODE_ENV === "production") {
-  db = new Client({
-    connectionString: getDatabaseUri(),
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
+if (process.env.NODE_ENV === "test") {
+  DB_URI = "postgresql:///jobly_test"
 } else {
-  db = new Client({
-    connectionString: getDatabaseUri()
-  });
-}
+  DB_URI = "postgresql:///jobly"
+};
+
+let db = new Client({
+  connectionString: DB_URI
+});
 
 db.connect();
 
 module.exports = db;
+
+
